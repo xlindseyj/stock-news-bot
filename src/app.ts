940bb2b9056c31e2a0d351327953ce26b5ffd317
xlindseyj <xlindseyj@outlook.com>
2021-05-06 21:35:05 -0400

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import cheerio from 'cheerio';
 import { Channel, Client, Message, TextChannel } from 'discord.js';
 // import { CommandHandler } from './discord/commands';
 import { discordConfig } from './discord/config';
 import { log } from './services/utilities.service';
 import { config } from './config';
-import { filter, forEach, map } from 'lodash';
+import { filter, forEach } from 'lodash';
 
 dotenv.config();
 
@@ -21,6 +21,10 @@ export default class Server {
 
   get isCryptoAllowed(): boolean {
     return config.isCryptoAllowed;
+  }
+
+  get isNewsAllowed(): boolean {
+    return config.isNewsAllowed;
   }
 
   get isNYSEWeek(): boolean {
@@ -44,12 +48,16 @@ export default class Server {
   }
 
   public getDiscordChannel = (channels: any, ticker: string, channelCategory: string): TextChannel => {
-    return channels.filter((channel: TextChannel) => channel.name === ticker && channel.parent.name === channelCategory);
+    return channels.filter((channel: TextChannel) => channel.name === ticker && channel.parent.name === channelCategory)[0];
   }
 
   public getOpenPrice = async (ticker: string): Promise<void> => {}
   public getClosePrice = async (ticker: string): Promise<void> => {}
   public getLatestNews = async (ticker: string): Promise<void> => {}
+
+  public getCurrentPriceMessage = (price: string): string => {
+    return `[${new Date().toLocaleTimeString()}]: The current price is ${price}`;
+  }
 
   public getCryptoPrice = async (url: string): Promise<string> => {
     let response: any = await axios(url)
@@ -66,13 +74,40 @@ export default class Server {
     return price; 
   }
 
+  public getTopStories = async (url: string, channel: TextChannel): Promise<any> => {
+    let response: any = await axios(url)
+    .catch((error) => log(error));
+
+    if (response.status !== 200){
+      throw Error('Error occurred while fetching data');
+    }
+
+    let news: string[] = [];
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const topStories = $('#yDmH0d > c-wiz > div > div.e1AOyf > div > div > div.fAThCb')
+      .find('.z4rs2b')
+      .toArray()
+      .map((links: any) => links.children[0].attribs.href);
+
+    const newsAlreadyInChatroom: string[] = await channel.messages.fetch({
+      limit: 100
+    }).then(messages => {
+      return [ ...messages ].map(([text, message]) => message.content);
+    });
+
+    news = filter(topStories, (link: string) => !newsAlreadyInChatroom.includes(link));
+
+    return news;
+  }
+
   public getStockNews = async (url: string, channel: TextChannel): Promise<any> => {
     let response: any = await axios(url)
       .catch((error) => log(error));
 
     if (response.status !== 200){
       throw Error('Error occurred while fetching data');
-
     }
 
     let news: string[] = [];
@@ -101,7 +136,6 @@ export default class Server {
 
     if (response.status !== 200){
       throw Error('Error occurred while fetching data');
-
     }
   
     const html = response.data;
@@ -119,7 +153,7 @@ export default class Server {
       log('Connecting to Google Finance...');
       log('Connecting to Coin Desk...', true);
       await this.refreshPrices();
-      setInterval(this.refreshPrices, 1000 * 60 * 5);
+      setInterval(this.refreshPrices, 1000 * 60 * 15); // every 15 minutes
     });
 
     this.discordClient.on('message', async (message: Message) => {});
@@ -150,23 +184,38 @@ export default class Server {
 
     if (this.isStocksAllowed) {
       /*
-        ##### GME News and Price #####
+        ##### GME Price #####
       */
-      channel = this.getDiscordChannel(channels, config.stocks[0], config.categories[2])[0];
-      news = await this.getStockNews(`${this.baseStockUrl}/quote/${config.stocks[0]}:NYSE`, channel);
       price = await this.getStockPrice(`${this.baseStockUrl}/quote/${config.stocks[0]}:NYSE`);
-      await this.postNews(news, channel);
-      channel = this.getDiscordChannel(channels, config.stocks[0], config.categories[3])[0];
-      await channel.send(price).catch((error: any) => log(error));
+      channel = this.getDiscordChannel(channels, config.stocks[0], config.categories[3]);
+      await channel.send(this.getCurrentPriceMessage(price)).catch((error: any) => log(error));
       /*
-        ##### MNMD News and Price #####
+        ##### MNMD Price #####
       */
-      channel = this.getDiscordChannel(channels, config.stocks[1], config.categories[2])[0];
-      news = await this.getStockNews(`${this.baseStockUrl}/quote/${config.stocks[1]}:NASDAQ`, channel);
       price = await this.getStockPrice(`${this.baseStockUrl}/quote/${config.stocks[1]}:NASDAQ`);
+      channel = this.getDiscordChannel(channels, config.stocks[1], config.categories[3]);
+      await channel.send(this.getCurrentPriceMessage(price)).catch((error: any) => log(error));
+    }
+
+    if (this.isNewsAllowed) {
+      /*
+        ##### Top Stories #####
+      */
+      channel = this.getDiscordChannel(channels, config.stocks[2], config.categories[2]);
+      news = await this.getTopStories(this.baseStockUrl, channel);
       await this.postNews(news, channel);
-      channel = this.getDiscordChannel(channels, config.stocks[1], config.categories[3])[0];
-      await channel.send(price).catch((error: any) => log(error));
+      /*
+        ##### GME News #####
+      */
+      channel = this.getDiscordChannel(channels, config.stocks[0], config.categories[2]);
+      news = await this.getStockNews(`${this.baseStockUrl}/quote/${config.stocks[0]}:NYSE`, channel);
+      await this.postNews(news, channel);
+      /*
+        ##### MNMD News #####
+      */
+      channel = this.getDiscordChannel(channels, config.stocks[1], config.categories[2]);
+      news = await this.getStockNews(`${this.baseStockUrl}/quote/${config.stocks[1]}:NASDAQ`, channel);
+      await this.postNews(news, channel);
     }
 
     if (this.isCryptoAllowed) {
@@ -175,25 +224,25 @@ export default class Server {
       */
       price = await this.getCryptoPrice(`${this.baseCryptoUrl}/${config.cryptos[0]}`);
       channel = this.getDiscordChannel(channels, config.cryptos[0], config.categories[0])[0];
-      await channel.send(price).catch((error: any) => log(error));
+      await channel.send(this.getCurrentPriceMessage(price)).catch((error: any) => log(error));
       /*
         ##### DOGE Price #####
       */
       price = await this.getCryptoPrice(`${this.baseCryptoUrl}/${config.cryptos[1]}`);
       channel = this.getDiscordChannel(channels, config.cryptos[1], config.categories[0])[0];
-      await channel.send(price).catch((error: any) => log(error));
+      await channel.send(this.getCurrentPriceMessage(price)).catch((error: any) => log(error));
       /*
         ##### ETH Price #####
       */
       price = await this.getCryptoPrice(`${this.baseCryptoUrl}/${config.cryptos[2]}`);
       channel = this.getDiscordChannel(channels, config.cryptos[2], config.categories[0])[0];
-      await channel.send(price).catch((error: any) => log(error));
+      await channel.send(this.getCurrentPriceMessage(price)).catch((error: any) => log(error));
       /*
         ##### XMR Price #####
       */
       price = await this.getCryptoPrice(`${this.baseCryptoUrl}/${config.cryptos[3]}`);
       channel = this.getDiscordChannel(channels, config.cryptos[3], config.categories[0])[0];
-      await channel.send(price).catch((error: any) => log(error));
+      await channel.send(this.getCurrentPriceMessage(price)).catch((error: any) => log(error));
     }
   }
 
