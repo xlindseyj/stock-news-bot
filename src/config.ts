888675ae46d136efbe838e0c4abc2b927524bac6
xlindseyj <xlindseyj@outlook.com>
2021-05-11 13:16:42 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,9 @@
 export const config = {
-    categories: ['General', 'News', 'Prices'],
+    categories: ['General', 'News', 'Prices', 'Reddit'],
     cryptos: ['btc', 'doge', 'eth', 'xmr'],
     news: ['top-stories', 'crypto-news', 'stock-news'],
     stocks: ['gme', 'mnmd'],
+    subreddits: ['wallstreetbets', 'pennystocks', 'gme', 'mnmd'],
     isCryptoAllowed: true,
     isNewsAllowed: true,
     isStocksAllowed: true
